e97ff46a9cce7aa5533677e2f109beaa66d4c2a9
Manas Nikhil <manasnikhil007@gmail.com>
2025-09-02 18:35:57 +0530

    Added Analog input interface project

---
+++ Analog_in_AI/Core/Inc/I2C_Master.h
@@ -0,0 +1,13 @@
+/*
+ * I2C_Master.h
+ *
+ *  Created on: Apr 3, 2025
+ *      Author: manas
+ */
+
+#ifndef INC_I2C_MASTER_H_
+#define INC_I2C_MASTER_H_
+
+void writeData(uint8_t *pdata, uint8_t len);
+
+#endif /* INC_I2C_MASTER_H_ */
