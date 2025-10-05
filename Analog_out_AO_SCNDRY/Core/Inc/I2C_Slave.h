9ce58245960ea32c82272b0502dcafa31ea2c40b
Manas Nikhil <manasnikhil007@gmail.com>
2025-10-05 20:53:34 +0530

    Code for the analog output module on the standby controller for simple parallel communication test

---
+++ Analog_out_AO_SCNDRY/Core/Inc/I2C_Slave.h
@@ -0,0 +1,6 @@
+#ifndef INC_I2C_SLAVE_H_
+#define INC_I2C_SLAVE_H_
+
+extern uint16_t analog_data;
+extern uint8_t rxBuffer[2];
+#endif /* INC_I2C_SLAVE_H_ */
---
+++ Analog_out_AO_SCNDRY/Core/Src/I2C_Slave.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include "I2C_Slave.h"
+
+extern I2C_HandleTypeDef hi2c1;
+
+uint16_t analog_data; // Single variable for analog value
+uint8_t rxBuffer[2];
+void HAL_I2C_AddrCallback(I2C_HandleTypeDef *hi2c, uint8_t TransferDirection, uint16_t AddrMatchCode) {
+  if (TransferDirection == I2C_DIRECTION_TRANSMIT) {
+    HAL_I2C_Slave_Seq_Receive_IT(&hi2c1, rxBuffer, 2, I2C_FIRST_AND_LAST_FRAME); // Get 2 bytes
+  } else {
+    Error_Handler();
+  }
+}
+
+void HAL_I2C_SlaveRxCpltCallback(I2C_HandleTypeDef *hi2c) {
+	analog_data = ((uint16_t) rxBuffer[0] << 8) | rxBuffer[1];
+	HAL_I2C_EnableListen_IT(&hi2c1);
+}
